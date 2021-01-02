90e04287935fe169cf969c6b3ec6a4f2d00ad7c4
rmh2805 <rmh2805@rit.edu>
2021-01-02 15:29:14 -0500

    Tested printGetStr and for board modification

--- testBoard.c
+++ testBoard.c
@@ -6,6 +6,8 @@
 #include "board.h"
 #include "chess.h"
 
+#define kBufSize 32
+
 int main() {
     board_t board = makeBoard();
     if(board == NULL) {
@@ -17,10 +19,23 @@ int main() {
         printf("Failed to initialize the display, exit failure\n");
         return EXIT_FAILURE;
     }
-    
-    setBoard(board);
+
+    char buf [kBufSize];
     
     printBoard(board);
+    printGetStr(buf, kBufSize);
+
+    setBoard(board);
+
+    printf("%s\n", buf);
+    printBoard(board);
+    printGetStr(buf, kBufSize);
+
+    setCell(board, 3, 3, kKingVal, true);
+    
+    printf("%s\n", buf);
+    printBoard(board);
+    
 
     delBoard(board);
     printf("test complete, exit success\n");
